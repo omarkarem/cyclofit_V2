78058dd1fb8baf5d76b09cf9d51576ba22f285ce
omarkarem <omar_karem156@outlook.com>
2025-04-28 20:47:05 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -78,11 +78,16 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
 
 // Serve static files (for legacy paths during migration period)
 app.use('/static', express.static(path.join(__dirname)));
