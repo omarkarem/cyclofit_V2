1b92df27baf9047385e88191739fbf3a00dcf566
omarkarem <omar_karem156@outlook.com>
2025-06-08 15:50:03 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -79,7 +79,7 @@ const app = express();
 app.use(cors({
   origin: ['https://cyclofit.grity.co', 'http://localhost:3000'],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
